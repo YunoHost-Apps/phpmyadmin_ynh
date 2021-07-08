1ed98e050f82e39715fd393ec7cf89990692e596
ericgaspar <junk.eg@free.fr>
2021-07-08 08:33:33 +0200

    Cleaning up

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,25 +5,12 @@
 #=================================================
 
 YNH_PHP_VERSION="7.3"
-extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
 
+extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
-
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-ynh_exec_as() {
-  local USER=$1
-  shift 1
-
-  if [[ $USER = $(whoami) ]]; then
-    eval "$@"
-  else
-    sudo -u "$USER" "$@"
-  fi
-}
 
 # Execute a command with Composer
 #
