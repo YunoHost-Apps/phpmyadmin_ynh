f90572a5d4b372a88287f21fa32771305e73afb5
ericgaspar <junk.eg@free.fr>
2022-05-11 19:50:40 +0200

    PHP8

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="8.0"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
 
-YNH_COMPOSER_VERSION=2.0.14
+YNH_COMPOSER_VERSION=2.3.5
 
 #=================================================
 # PERSONAL HELPERS
