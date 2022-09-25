63717d4b8235b1a8f9df826148bc8bfcc5263b61
ericgaspar <junk.eg@free.fr>
2022-09-25 22:11:49 +0200

    5.2.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ YNH_PHP_VERSION="7.4"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
 
-YNH_COMPOSER_VERSION=2.0.14
+YNH_COMPOSER_VERSION=2.4.2
 
 #=================================================
 # PERSONAL HELPERS
