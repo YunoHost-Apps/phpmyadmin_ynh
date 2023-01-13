3ba27cd14d33a491f77dbd8417d8fa97fbf4be0c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-13 09:15:44 +0100

    5.2.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
+
+YNH_COMPOSER_VERSION=2.4.2
 
 pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
-
-YNH_COMPOSER_VERSION=2.0.14
 
 #=================================================
 # PERSONAL HELPERS
