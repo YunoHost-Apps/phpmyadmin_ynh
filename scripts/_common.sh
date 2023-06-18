6b93c09870eba2623b065db4e2c1eb9f4281af7d
ericgaspar <junk.eg@free.fr>
2023-06-18 09:34:41 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="8.2"
 
-YNH_COMPOSER_VERSION=2.4.2
+YNH_COMPOSER_VERSION=2.5.7
 
 pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
 
