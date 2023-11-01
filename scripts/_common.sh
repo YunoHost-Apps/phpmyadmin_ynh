1551eadd7548a3f6b582cf0e27c80fdaa7448c4a
Mateusz <2871798+orhtej2@users.noreply.github.com>
2023-11-01 23:59:18 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.4"
-
-#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mbstring"
-
 YNH_COMPOSER_VERSION=2.0.14
 
 #=================================================
